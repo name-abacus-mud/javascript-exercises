ef4feb4ba5f5558e4d51fd26307c5dda673b27b7
taha <297468732+name-abacus-mud@users.noreply.github.com>
2026-07-06 07:06:07 +0530

    solved the tempConversion ex!!
    really happy i did this one all by myself!!

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let getCelsius=(fahrenheit-32) * 5/9;
+  // formula to convert fahrenheit to celsius
+  getCelsius=Math.round(getCelsius*10)/10;
+  // rounding the decimal place to one 
+  return getCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let getFahrenheit=(celsius*9/5+32);
+  // formula to convert celsius to fahrenheit
+  getFahrenheit=Math.round(getFahrenheit*10)/10;
+  // rounding the decimal place to one
+  return getFahrenheit;
 };
 
 // Do not edit below this line
