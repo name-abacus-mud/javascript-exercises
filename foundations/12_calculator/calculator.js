8eeddd32755a7ab770ed35d68a440b6fc5bb0c09
taha <297468732+name-abacus-mud@users.noreply.github.com>
2026-07-10 18:59:39 +0530

    Finished this calculator exercise !!
    it was nice to do this exercise!! not so hard!!

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sumOfAllNum = array.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+
+  return sumOfAllNum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  const productOfAllNum = array.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 
+  return productOfAllNum;
 };
 
-const power = function() {
-	
+const power = function(firstNum, secondNum) {
+	return firstNum ** secondNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product = 1;
+
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
