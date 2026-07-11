120883a175f3031a51f38ef52ac7c7c8adc9412b
taha <297468732+name-abacus-mud@users.noreply.github.com>
2026-07-11 21:51:21 +0530

    Finished all the exercises

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const listOFAllowedChar = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter(character => listOFAllowedChar.includes(character))
+    .join('');
+
+    const reverseString = cleanedString.split('').reverse().join('');
+
+    return cleanedString == reverseString;
 };
 
 // Do not edit below this line
--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let count = +input;
 
+    if (count < 0) return 'OOPS';
+    if (count == 0 ) return 0;
+
+    let firstNum = 1;
+    let secondNum = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstNum + secondNum;
+        secondNum = firstNum;
+        firstNum = current;
+    }
+
+    return firstNum;
 };
 
 // Do not edit below this line
